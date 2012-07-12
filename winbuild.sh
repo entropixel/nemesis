072a52de1b601d0e7aca63ec8dd700d6322bed7f
tm512 <elykdav@gmail.com>
2012-07-11 18:23:21 -0700

    Fix winbuild.sh

--- winbuild.sh
+++ winbuild.sh
@@ -3,9 +3,9 @@
 SDL2=/home/kyle/win32-sdl2
 zipname=nem-win32-$(date +"%m%d%y-%H%M").zip
 
-make CC=i686-w64-mingw32-gcc SDL2_PATH=$SDL2 OUT=nemesis.exe
+make CC=i686-w64-mingw32-gcc SDL2_PATH=$SDL2 OUT=nemesis.exe OPT=2 DBG=0
 i686-w64-mingw32-strip nemesis.exe
 
-zip /tmp/$zipname nemesis.exe img/*
+zip -r /tmp/$zipname nemesis.exe img/*
 cd $SDL2/bin
 zip /tmp/$zipname *dll
