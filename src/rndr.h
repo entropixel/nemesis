422e2bf5147840fa71b03143deb4ce22d9458373
tm512 <elykdav@gmail.com>
2012-07-16 13:09:42 -0700

    Seperate rndr_ functions into rndr.[ch]. Needs more work to seperate all the render stuff from the main loop though.

---
+++ src/rndr.h
@@ -0,0 +1,12 @@
+/* nemesis engine
+   [c] 2012 Kyle Davis, All Rights Reserved
+   Unauthorized redistribution is prohibited. */
+
+#ifndef RNDR_H__
+#define RNDR_H__
+
+void rndr_rgb_to_hsl (unsigned char *res, unsigned char r, unsigned char g, unsigned char b);
+void rndr_hsl_to_rgb (unsigned char *res, unsigned char h, unsigned char s, unsigned char l);
+void rndr_shift_sprite (SDL_Surface *spr, unsigned char alpha, char shift);
+
+#endif // RNDR_H__
