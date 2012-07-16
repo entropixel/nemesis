28a89584b8f5d4bd7f930e4269a82dc74fbde02b
tm512 <elykdav@gmail.com>
2012-07-16 12:20:04 -0700

    Sprite color shifting, and HSL/RGB conversion functions to go along with it

--- src/main.c
+++ src/main.c
@@ -50,6 +50,124 @@ enum
 
 unsigned int keymask = 0;
 
+// alphas: 
+// hair - 232
+// eyes - 242
+// shirt - 237
+// pants - 236
+
+void rndr_rgb_to_hsl (unsigned char *res, unsigned char r, unsigned char g, unsigned char b)
+{
+	unsigned char h, s, l;
+	unsigned char max = (((r > g) ? r : g) > b) ? ((r > g) ? r : g) : b;
+	unsigned char min = (((r < g) ? r : g) < b) ? ((r < g) ? r : g) : b;
+	unsigned char del = max - min;
+	
+	l = (min + max) / 2;
+	s = 0;
+
+	if (l > 0 && l < 255)
+		s = (unsigned char)(((unsigned int)255 * del) / ((l > 128) ? (512 - del) : (max + min)));
+
+	if (del)
+	{
+		if (max == r)
+			h = 43 * (g - b) / del;
+		else if (max == g)
+			h = 85 + (43 * (b - r) / del);
+		else
+			h = 171 + (43 * (r - g) / del);
+	}
+
+	res [0] = h;
+	res [1] = s;
+	res [2] = l;
+
+	return;
+}
+
+#define HSLMUL(x,y) ((((unsigned int)(x)) * ((unsigned int)(y))) / 256)
+#define HSLDIV(x,y) ((((unsigned int)(x)) * 256) / ((unsigned int)(y)))
+
+void rndr_hsl_to_rgb (unsigned char *res, unsigned char h, unsigned char s, unsigned char l)
+{
+	unsigned int sr, sg, sb, tmpr, tmpg, tmpb;
+	register unsigned int hh, ss, ll;
+
+	hh = h;
+	ss = s;
+	ll = l;
+
+	if (hh < 85)
+	{
+		sr = HSLDIV (85 - hh, 43);
+		sg = HSLDIV (hh, 43);
+		sb = 0;
+	}
+	else if (hh < 171)
+	{
+		sr = 0;
+		sg = HSLDIV (171 - hh, 43);
+		sb = HSLDIV (hh - 85, 43);
+	}
+	else
+	{
+		sr = HSLDIV (hh - 171, 43);
+		sg = 0;
+		sb = HSLDIV (256 - hh, 43);
+	}
+
+	sr = (sr < 256) ? sr : 256;
+	sg = (sg < 256) ? sg : 256;
+	sb = (sb < 256) ? sb : 256;
+
+	tmpr = HSLMUL (512, HSLMUL (ss, sr)) + (256 - ss);
+	tmpg = HSLMUL (512, HSLMUL (ss, sg)) + (256 - ss);
+	tmpb = HSLMUL (512, HSLMUL (ss, sb)) + (256 - ss);
+
+	if (ll < 128)
+	{
+		res[0] = HSLMUL (ll, tmpr);
+		res[1] = HSLMUL (ll, tmpg);
+		res[2] = HSLMUL (ll, tmpb);
+	}
+	else
+	{
+		res[0] = HSLMUL (256 - ll, tmpr) + HSLMUL (512, ll - 256);
+		res[1] = HSLMUL (256 - ll, tmpg) + HSLMUL (512, ll - 256);
+		res[2] = HSLMUL (256 - ll, tmpb) + HSLMUL (512, ll - 256);
+	}
+
+	return;
+}
+
+// shifts all pixels in the sprite that have the specified alpha value
+void rndr_shift_sprite (SDL_Surface *spr, unsigned char alpha, char shift)
+{
+	SDL_PixelFormat *f = spr->format;
+	int i, j;
+
+	for (i = 0; i < spr->w; i++)
+		for (j = 0; j < spr->h; j++)
+		{
+			unsigned int *pix = spr->pixels + (j * spr->w * f->BytesPerPixel) + (i * f->BytesPerPixel);
+
+			if ((*pix & f->Amask) >> f->Ashift == alpha)
+			{
+				unsigned char hsl [3];
+				unsigned char rgb [3];
+				
+				// convert pixel's rgb to hsl, shift, and reconvert
+				rndr_rgb_to_hsl (hsl, (*pix & f->Rmask) >> f->Rshift, (*pix & f->Gmask) >> f->Gshift, (*pix & f->Bmask) >> f->Bshift);
+				hsl [0] += shift; // shift
+				rndr_hsl_to_rgb (rgb, hsl [0], hsl [1], hsl [2]);
+				*pix = (rgb [0] << f->Rshift) | (rgb [1] << f->Gshift) | (rgb [2] << f->Bshift) | (255 << f->Ashift);
+			} 
+		}
+
+	return;
+}
+
 int main (int argc, char **argv)
 {
 	if (SDL_Init (SDL_INIT_VIDEO) < 0)
@@ -78,8 +196,12 @@ int main (int argc, char **argv)
 	}
 
 	// testing crap now :D
-	SDL_Surface *plsprite = IMG_Load ("img/objects/player/male/unarm/nosleeve_shorts_short.png");
-	SDL_Surface *torchspr = IMG_Load ("img/objects/dungeon/adungeon/torches.png");
+	SDL_Surface *plsprite = IMG_Load ("img/objects/player/male/nosleeve_shorts_short.png");
+	rndr_shift_sprite (plsprite, 232, 20);
+/*	rndr_shift_sprite (plsprite, 242, 110);
+	rndr_shift_sprite (plsprite, 237, 85);
+	rndr_shift_sprite (plsprite, 236, 90);
+*/	SDL_Surface *torchspr = IMG_Load ("img/objects/dungeon/adungeon/torches.png");
 	SDL_Surface *tilesheet = IMG_Load ("img/tiles/dungeon/adungeon.png");
 	obj_t *player = obj_create (32, 32, SDL_CreateTextureFromSurface (rndr, plsprite), &char_anim, char_anim_stand, ROT_DOWNRIGHT);
 	obj_t *torch = obj_create (128, 16, SDL_CreateTextureFromSurface (rndr, torchspr), &torch_anim, 0, ROT_DOWN);
