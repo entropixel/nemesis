06a144157d92f70400815389ce648d5d43a13668
tm512 <elykdav@gmail.com>
2012-09-24 10:39:35 -0700

    Some fixed point stuff.

---
+++ src/fixed.c
@@ -0,0 +1,20 @@
+/* nemesis engine
+   [c] 2012 Kyle Davis, All Rights Reserved
+   Unauthorized redistribution is prohibited. */
+
+#include "int.h"
+
+inline fixed float_to_fixed (float a)
+{
+	return (fixed)(a * (1 << FRAC));
+}
+
+inline fixed fixmul (fixed a, fixed b)
+{
+	return (fixed)((uint32) a * b >> FRAC);
+}
+
+inline fixed fixdiv (fixed a, fixed b)
+{
+	return (fixed)((uint32) a / b << FRAC);
+}
--- src/int.h
+++ src/int.h
@@ -10,5 +10,11 @@ typedef int32_t int32;
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
+typedef uint16_t fixed;
+
+#define FRAC 4 // 16 sub-units
+fixed float_to_fixed (float a);
+fixed fixmul (fixed a, fixed b);
+fixed fixdiv (fixed a, fixed b);
 
 #endif // INT_H__
